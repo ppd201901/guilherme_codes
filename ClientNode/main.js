a250ffd323f68b8326b58a08bc093483381a2282
Guilherme Ribeiro <guilherme.ufma@gmail.com>
2019-05-03 13:56:38 -0300

    Cliente (node) e servidor (python) comunicando via RPC (ex. calc_peso).

--- ClientNode/main.js
+++ ClientNode/main.js
@@ -4,12 +4,21 @@
  * and open the template in the editor.
  */
 
+/**
+ *
+ * @author guilhermeribeiro
+ */
+
 var zerorpc = require("zerorpc");
 
 var client = new zerorpc.Client();
 client.connect("tcp://127.0.0.1:4242");
 
-client.invoke("hello", "RPC", "2", function(error, res, more) {
+/*client.invoke("hello", "RPC", "2", function(error, res, more) {
     console.log(res);
+});*/
+
+client.invoke("calc_peso", "1.68", "masculino", function(error, res, more) {
+    console.log("Seu peso ideal deve ser: ", parseFloat(res));
 });
 
--- ClientNode/server_rpc.py
+++ ClientNode/server_rpc.py
@@ -4,6 +4,16 @@ class HelloRPC(object):
     def hello(self, name, number):
         return "Hello, {} {}".format(name, number)
 
+    def calc_peso(self, alt, sex):
+        peso = 0
+        alt = float(alt)
+
+        if sex == "masculino":
+            peso = (72.7*alt)-58
+        elif sex == "feminino":
+            peso = (62.1 * alt) - 44.7
+        return peso
+
 s = zerorpc.Server(HelloRPC())
 s.bind("tcp://0.0.0.0:4242")
 s.run()
