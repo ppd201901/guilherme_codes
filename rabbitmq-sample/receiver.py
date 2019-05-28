17ca329abb83c1e95bd73abd7e8d6c05ee0173a6
Guilherme Ribeiro <guilherme.ufma@gmail.com>
2019-05-28 19:41:34 -0300

    Ajuste para produzir 100 e consumir 100 mensagens.

--- rabbitmq-sample/receiver.py
+++ rabbitmq-sample/receiver.py
@@ -1,19 +1,19 @@
 # Para subir o Rabbitmq server (com o gerenciador das filas), basta executar o comando
-#  rabbitmq-server no terminal.
+# sbin/rabbitmq-server no terminal, dentro da pasta /usr/local.
+# lista filas: sbin/rabbitmqctl list_queues
+
 import pika
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
-
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 
-channel.basic_consume(e_callback=callback, auto_ack=True)
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- rabbitmq-sample/sender.py
+++ rabbitmq-sample/sender.py
@@ -1,14 +1,17 @@
 # Para subir o Rabbitmq server (com o gerenciador das filas), basta executar o comando
-#  rabbitmq-server no terminal.
+#  sbin/rabbitmq-server no terminal, dentro da pasta /usr/local.
+# lista filas: sbin/rabbitmqctl list_queues
 
 import pika
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host="localhost"))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+
+for x in range(10000):
+    channel.basic_publish(exchange='', routing_key='hello', body=str(x) + ' Hello World!')
+    print(str(x) + " [x] Sent 'Hello World!'")
+
 connection.close()
